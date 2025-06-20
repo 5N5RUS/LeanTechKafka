320ba3c8b1680717d480b61f4e446c099c8acb77
Fendel Nikita <fendelnikita@gmail.com>
2025-06-20 16:00:57 +0600

    feat: init project

---
+++ src/main/java/ru/fendel/leantechkafka/LeanTechKafkaApplication.java
@@ -0,0 +1,13 @@
+package ru.fendel.leantechkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanTechKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeanTechKafkaApplication.class, args);
+    }
+
+}
